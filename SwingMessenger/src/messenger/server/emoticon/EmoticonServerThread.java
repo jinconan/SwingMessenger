be46524e5b1aeba435a8c2d63907e4401d3fe274
jinlee <jinconan00@naver.com>
2018-06-25 22:17:24 +0900

    클래스 다이어그램 추가
    이모티콘은 DB에 저장하지 않았으므로 DAO 제거하고 메소드를 이모티콘 서버 쓰레드로 옮김

--- SwingMessenger/src/messenger/server/emoticon/EmoticonServerThread.java
+++ SwingMessenger/src/messenger/server/emoticon/EmoticonServerThread.java
@@ -1,18 +1,24 @@
 package messenger.server.emoticon;
 
+import java.awt.Font;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
 
-import messenger._db.dao.EmoticonDAO;
 import messenger._db.dao.LoginDAO;
 import messenger._db.vo.MemberVO;
 import messenger.protocol.Message;
@@ -47,10 +53,9 @@ public class EmoticonServerThread implements Runnable{
 					ObjectOutputStream		oout	= new ObjectOutputStream(bout);
 				){
 					System.out.println("socket.getOutputStream()");
-					EmoticonDAO dao = EmoticonDAO.getInstance();
 					System.out.println("DAO.getInstance()");
 					
-					ArrayList<JLabel> response = dao.getEmoticonList();
+					ArrayList<JLabel> response = getEmoticonList();
 					msg.setResponse(response);
 					oout.writeObject(msg);
 					oout.flush();
@@ -73,4 +78,43 @@ public class EmoticonServerThread implements Runnable{
 			}
 	}
 
+	/**
+ 	 * 내부 저장소에서 이모티콘 파일을 전부 불러와서 리스트로 변환하는 메소드
+ 	 * @return JLabel로 만든 이모티콘 리스트
+ 	 */
+ 	public ArrayList<JLabel> getEmoticonList() {
+ 		ArrayList<JLabel> list = new ArrayList<JLabel>(); //이미지 아이콘 리스트
+ 		
+ 		//클래스파일의 기본 경로를 가져온다.
+ 		ClassLoader loader = this.getClass().getClassLoader();
+ 		
+ 		//위에서 얻은 경로를 시작지점으로 하여 이모티콘이 담긴 폴더의 상대경로 얻기.
+		URL buildpath = loader.getResource("messenger\\server\\emoticon\\images\\");
+		try {
+			URI uri = new URI(buildpath.toString());
+			File file = new File(uri); //이모티콘 폴더를 인스턴스화하기
+			
+			//폴더 경로 + 이모티콘 이름을 이어붙여서 이미지 아이콘 인스턴스화
+			for(String s :file.list()) {
+				StringBuilder emoticonPath = new StringBuilder(uri.getPath());
+				emoticonPath.append(s);
+				StringBuilder emoticonName = new StringBuilder("(");
+				String[] token = s.split("\\.");
+				emoticonName.append(token[0]);
+				emoticonName.append(")");
+				
+				ImageIcon icon = new ImageIcon(emoticonPath.toString());
+				JLabel iconLabel = new JLabel(emoticonName.toString(), icon, SwingConstants.CENTER);
+				iconLabel.setFont(new Font("굴림", Font.BOLD,0));
+				list.add(iconLabel);
+			}
+			
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		
+		return list;
+ 		
+ 	}
+	
 }

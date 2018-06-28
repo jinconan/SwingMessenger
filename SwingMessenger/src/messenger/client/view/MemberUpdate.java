967afec7bcb307ae76ecb62c5d187f02ebb35e8b
516 <516@DESKTOP-2DQ28JO>
2018-06-28 15:35:06 +0900

    [김진혁] 메인UI(ClientFrame) 서브 창(SubMenu) 중복검사 클라이언트 구현, 회원정보수정 클라이언트 구현

--- SwingMessenger/src/messenger/client/view/MemberUpdate.java
+++ SwingMessenger/src/messenger/client/view/MemberUpdate.java
@@ -2,51 +2,59 @@ package messenger.client.view;
 
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 import messenger._db.vo.MemberVO;
 import messenger.protocol.Message;
 
 public class MemberUpdate<T> {
-	
+	Socket socket = new Socket();
 	MemberVO mvo = new MemberVO();
 	ObjectOutputStream oos = null;
 	ObjectInputStream ois = null;
 	SubMenu sm = new SubMenu();
 	MemberVO mem = new MemberVO();
-	Message<T> info = null;
-	
+
 	public void Certified() {
 		try {
-			ois.readObject();
-			if(sm.jtf_upw.getText() == mem.getMem_pw()) {
+			socket = new Socket("",);
+			oos = new ObjectOutputStream(socket.getOutputStream());
+			oos.writeObject(sm.jtf_upw.getText());
+			ois = new ObjectInputStream(socket.getInputStream());
+			if(sm.jtf_upw.getText() == ois.readObject()) {
 				sm.UpdateInfo();
+			}else {
+				JOptionPane.showMessageDialog(sm.jtf_upw, "잘못된 비밀번호입니다.", "Error", JOptionPane.ERROR_MESSAGE);
 			}
 		} catch (Exception e) {
 			// TODO: handle exception
-		}
-	}
-	@SuppressWarnings("unchecked")
+			e.printStackTrace();
+		}///////// end try catch
+		
+	}///////// end Certified()
+
 	public void Update() {
-		//String info = null;
-		Client client = new Client();
+		
 		try {
-			client.oos.writeObject(mem.getMem_id());
+			socket = new Socket("",);
+			oos = new ObjectOutputStream(socket.getOutputStream());
+			oos.writeObject(mem);
+			//MemberVO에 수정한 회원정보 담기
+			mem.setMem_nick(sm.jtf_unick.getText());
 			mem.setMem_pw(sm.jtf_upw.getText());
 			mem.setMem_name(sm.jtf_uname.getText());
-			//mem.setMem_gender(sm.jtf_ugender);
+			mem.setMem_gender((String)sm.jtf_ugender.getSelectedItem());
 			mem.setMem_hp(sm.jtf_uhp.getText());
-			info.setType(2);
-			List<MemberVO> Lmem = new ArrayList<MemberVO>();
-			info.setResponse((List<T>) Lmem);
 			
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
-		}
+		}///////// end try catch
 
-	}
-}
+	}///////// end Update()
+
+}///////// end class

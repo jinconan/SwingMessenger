83f0c1a781b520c506e1807f183ce08da2109aed
jinlee <jinconan00@naver.com>
2018-06-29 02:10:04 +0900

    MemberVO 전용으로 커스텀한 테이블 클래스 및 테이블모델 클래스 추가(MemberVOTable, MemberVOTableModel)
    사용법은 똑같이 테이블 모델을 만들고 테이블에 부착하면 끝.(MemberVOCell은 테이블 내에서 처리를 하기 위해 만들어졌고
    직접 사용하지는 않음.)

---
+++ SwingMessenger/src/messenger/client/view/MemberVOTable.java
@@ -0,0 +1,27 @@
+package messenger.client.view;
+
+import javax.swing.DefaultListSelectionModel;
+import javax.swing.JTable;
+
+import messenger._db.vo.MemberVO;
+
+public class MemberVOTable extends JTable{
+	
+	public MemberVOTable() {
+		super();
+		setDefault();
+	}
+	
+	public MemberVOTable(MemberVOTableModel model) {
+		super(model);
+		setDefault();
+	}
+	
+	private void setDefault() {
+		MemberVOCell memberVOCell = new MemberVOCell();
+		setDefaultRenderer(MemberVO.class, memberVOCell);
+		setDefaultEditor(MemberVO.class, memberVOCell);
+		setSelectionMode(DefaultListSelectionModel.SINGLE_SELECTION);
+		setRowHeight(60);
+	}
+}
---
+++ SwingMessenger/src/messenger/client/view/MemberVOTableModel.java
@@ -0,0 +1,53 @@
+package messenger.client.view;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+import messenger._db.vo.MemberVO;
+
+public class MemberVOTableModel extends AbstractTableModel{
+	private List<MemberVO> list;
+	private String columnName;
+	
+	public MemberVOTableModel() {
+		this(new ArrayList<MemberVO>(), null);
+	}
+	
+	public MemberVOTableModel(List<MemberVO> list) {
+		this(list, null);
+	}
+
+	public MemberVOTableModel(List<MemberVO> list, String columnName) {
+		this.list = list;
+		this.columnName = columnName;
+	}
+	
+	@Override
+	public Class<?> getColumnClass(int columnIndex) {
+		return MemberVO.class;
+	}
+	@Override
+	public String getColumnName(int columnIndex) {
+		return columnName;
+	}
+	@Override
+	public int getColumnCount() {
+		return 1;
+	}
+	
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return list.size();
+	}
+	
+	@Override
+	public MemberVO getValueAt(int row, int col) {
+		// TODO Auto-generated method stub
+		return (list.size()-1 >= row) ? list.get(row) : null;
+	}
+
+	
+}

c43f16191ad5b897fc90460ce73ab1097c072a83
Jeongryeol Lee <duxbellorn@gmail.com>
2018-06-29 17:46:07 +0900

    이정렬/Client : test누락 추가

--- SwingMessenger/src/messenger/client/friend/test.java
+++ SwingMessenger/src/messenger/client/friend/test.java
@@ -3,7 +3,10 @@ package messenger.client.friend;
 public class test {
 
 	public static void main(String[] args) {
-		ClientFriendList cfl = new ClientFriendList(35);
-		cfl.getFriendList();
+		//ClientFriendList cfl = new ClientFriendList(35);
+		//cfl.getFriendList();
+		
+		ClientFriendSearch cfc = new ClientFriendSearch("weback");
+		cfc.getFriendSearch();
 	}
 }

07ebd190c42129af29b6318632e59d709e5e08d3
512 <512@DESKTOP-20LF68C>
2018-07-04 11:31:38 +0900

    오류 수정

--- SwingMessenger/src/messenger/client/view/Joongbok.java
+++ SwingMessenger/src/messenger/client/view/Joongbok.java
@@ -51,17 +51,19 @@ public class Joongbok {
 				try {
 					if (message.getResponse() == null) {
 						JOptionPane.showMessageDialog(cf.jtf_gid, "이미 사용중인 아이디입니다.", "Error", JOptionPane.ERROR_MESSAGE);
-					}else {
-						if (cf.jtf_gid.getText() == message.getResponse().get(0).getMem_id()) {
+						answer=3;
+					}else if(message.getResponse()!=null){
+						answer=1;
+						/*if (cf.jtf_gid.getText() == message.getResponse().get(0).getMem_id()) {
 							answer = JOptionPane.showConfirmDialog(cf.jtf_gid, "이 아이디를 사용하시겠습니까?", "사용가능한 아이디입니다.",
-									JOptionPane.YES_NO_OPTION);
+								    	JOptionPane.YES_NO_OPTION);
 							if (answer == JOptionPane.YES_OPTION) {
 								answer = 0;
 							} else if (answer == JOptionPane.NO_OPTION) {
 								cf.jtf_gid.setText("");
 							}
 							System.out.println(answer);
-						}
+						}*/
 					}
 					
 				} catch (NullPointerException e) {
